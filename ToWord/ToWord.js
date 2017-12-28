5423a6754da067884c01bfeb6659655b6d663b8b
R2bEEaton <34921506+R2bEEaton@users.noreply.github.com>
2017-12-28 14:24:17 -0500

    Add files via upload

--- ToWord/ToWord.js
+++ ToWord/ToWord.js
@@ -3,36 +3,20 @@ function round(value) {
 }
 
 function ToWord(number) {
-var placevalues = ["blank", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion", "Decillion", "Undecillion", "Duodecillion", "Tredecillion", "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septendecillion", "Octodecillion", "Novemdecillion", "Vigintillion", "Unvigintillion"];
+var placevalues = ["blank", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion", "Decillion", "Undecillion", "Duodecillion", "Tredecillion", "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septendecillion", "Octodecillion", "Novemdecillion", "Vigintillion", "Unvigintillion", "Duovigintillion", "Tresvigintillion", "Quattuorvigintillion", "Quinvigintillion", "Sexvigintillion", "Septenvigintillion", "Octovigintillion", "Novemvigintillion", "Trigintillion", "Untrigintillion", "Duotrigintillion", "Tretrigintillion", "Quattuortrigintillion", "Quintrigintillion", "Sextrigintillion", "Septentrigintillion", "Octotrigintillion", "Novemtrigintillion", "Quadragintillion", "Unquadragintillion", "Duoquadragintillion", "Trequadragintillion", "Quattuorquadragintillion", "Quinquadragintillion", "Sexquadragintillion", "Septenquadragintillion", "Octoquadragintillion", "Novemquadragintillion"];
     if (((Math.round(number)).toString().length) > 3) {
         var length = (number.toString().length) % 3;
         if (length == 0) {length = (number.toString().length) - ((number.toString().length) - 3);}
         var rl = (number.toString().length) - length;
         var d = round(number / (10**(rl)));
         var pre = Math.round(number / (10**(rl)));
-        if (pre.toString().length > 3) {
+        while (pre.toString().length > 3) {
             var length = (pre.toString().length) % 3;
             if (length == 0) {length = (pre.toString().length) - ((pre.toString().length) - 3);}
             var nl = (pre.toString().length) - length;
             var rl = rl + nl;
             var d = round(number / (10**(rl)));
             var pre = Math.round(pre / (10**(nl)));
-                if (pre.toString().length > 3) {
-                var length = (pre.toString().length) % 3;
-                if (length == 0) {length = (pre.toString().length) - ((pre.toString().length) - 3);}
-                var nl = (pre.toString().length) - length;
-                var rl = rl + nl;
-                var d = round(number / (10**(rl)));
-                var pre = Math.round(pre / (10**(nl)));
-                    if (pre.toString().length > 3) {
-                    var length = (pre.toString().length) % 3;
-                    if (length == 0) {length = (pre.toString().length) - ((pre.toString().length) - 3);}
-                    var nl = (pre.toString().length) - length;
-                    var rl = rl + nl;
-                    var d = round(number / (10**(rl)));
-                    var pre = Math.round(pre / (10**(nl)))}
-                    
-        }
         }
         return d + " " + placevalues[rl / 3];
     } else return number;
